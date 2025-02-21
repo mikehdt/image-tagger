cb5f5c7c61b7a2717b3e9d70b0fc1c4e1a8f723e
Mike Hopkins <mike@dthought.net>
2025-02-22 09:10:52 +1100

    Upgrade Tailwind

--- src/app/tags/tag-actions.tsx
+++ src/app/tags/tag-actions.tsx
@@ -6,13 +6,13 @@ type TagActionsProps = {
 export const TagActions = ({ clearChanges, saveFilters }: TagActionsProps) => (
   <span className="ml-auto">
     <button
-      className="rounded-sm bg-slate-200 px-4 py-1 hover:bg-slate-400"
+      className="rounded-xs bg-slate-200 px-4 py-1 hover:bg-slate-400"
       onClick={clearChanges}
     >
       Cancel
     </button>
     <button
-      className="ml-2 rounded-sm bg-emerald-200 px-4 py-1 hover:bg-emerald-400"
+      className="ml-2 rounded-xs bg-emerald-200 px-4 py-1 hover:bg-emerald-400"
       onClick={saveFilters}
     >
       Save
