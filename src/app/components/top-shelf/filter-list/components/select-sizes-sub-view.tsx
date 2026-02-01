45ef70b56ab078d9cdc1df212bc49a28caceff77
Mike Hopkins <mike@dthought.net>
2026-02-01 22:10:36 +1100

    Style fix

--- src/app/components/top-shelf/filter-list/components/select-sizes-sub-view.tsx
+++ src/app/components/top-shelf/filter-list/components/select-sizes-sub-view.tsx
@@ -33,7 +33,7 @@ export const SizeSubViewSelector = () => {
   };
 
   return (
-    <div className="flex w-full items-center rounded-sm shadow-md inset-shadow-xs shadow-white inset-shadow-slate-300 dark:shadow-slate-900 dark:inset-shadow-slate-600">
+    <div className="flex w-full items-center rounded-sm shadow-md inset-shadow-xs shadow-white inset-shadow-slate-300 dark:shadow-slate-900/50 dark:inset-shadow-slate-600">
       <button
         type="button"
         onClick={() => handleSubViewChange('dimensions')}
